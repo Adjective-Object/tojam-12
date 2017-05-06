b2121669853ab7af6700749ec742891166b166c8
Artur Kink <artur.kink@gmail.com>
2017-05-06 13:10:18 -0400

    Network player identification

--- TOJAM12/GameInstance.cs
+++ TOJAM12/GameInstance.cs
@@ -9,19 +9,31 @@ namespace TOJAM12
     public class GameInstance
     {
         Network network;
-        bool isServer;
         TojamGame game;
+
+        Player[] players;
+        
         public GameInstance(TojamGame game)
         {
             this.game = game;
-            isServer = true;
             network = new Network();
-            network.Start(isServer);
+            network.Start(Console.ReadKey().Key == ConsoleKey.S);
+
+            players = new Player[4];
         }
         public void Update()
         {
+            if (network.IsServer())
+            {
+                // Parse server generated messages
+                foreach (Command command in network.GetLocalCommands())
+                {
+                    ParseCommand(command);
+                }
+            }
+
             network.Update();
-            foreach(String command in network.GetCommands())
+            foreach(Command command in network.GetCommands())
             {
                 ParseCommand(command);
             }
@@ -29,25 +41,54 @@ namespace TOJAM12
 
         public void SendPlayerCommand(String command)
         {
-            if (isServer)
+            if (network.IsServer())
             {
-                ParseCommand(command);
+                ParseCommand(new Command(Command.CommandType.Player, command, 1));
             }
             else
             {
-                network.SendCommand(command);
+                network.SendCommand(new Command(Command.CommandType.Player, command, 0));
             }
         }
 
-        private void ParseCommand(String command)
+        private void SendTextCommand(String command, int player)
+        {
+
+        }
+
+        private void ParseCommand(Command command)
         {
             //Decide what to do with command
-            Console.WriteLine("Parsing command '" + command + "'");
+            Console.WriteLine("Parsing command " + command.Type + " from Player " + command.PlayerId + " '" + command.Data + "'");
 
-            ((ChatScene)game.GetScene(TojamGame.GameScenes.Chat)).AddMessage(command);
+            // Player command is a command issued by the player
+            // Only handled by server
+            if (network.IsServer() && command.Type == Command.CommandType.Player)
+            {
+                command.Data = command.Data.Trim();
+                String[] tokens = command.Data.Split(' ');
+                if (tokens.Length > 0)
+                {
+                    if (tokens[0] == "SAY")
+                    {
+                        if (tokens.Length == 1)
+                            network.SendCommand(new Command(Command.CommandType.Text, command.PlayerId + " said nothing", 0));
+                        else
+                            network.SendCommand(new Command(Command.CommandType.Text, command.PlayerId + " said '" + command.Data.Substring(4) + "'", 0));
+                    }
+                    else
+                    {
+                        network.SendCommand(new Command(Command.CommandType.Text, "Unknown Command", command.PlayerId));
+                    }
+                }
+            }
+            else if (command.Type == Command.CommandType.Text)
+            {
+                ((ChatScene)game.GetScene(TojamGame.GameScenes.Chat)).AddMessage(command.Data);
+            }
 
-            if (isServer)
-                network.SendCommand(command);
+            
+            
         }
     }
 }
--- TOJAM12/Network.cs
+++ TOJAM12/Network.cs
@@ -9,13 +9,30 @@ namespace TOJAM12
 {
     public class Network
     {
+        bool isServer;
+        public bool IsServer()
+        {
+            return isServer;
+        }
         NetServer server;
+
         NetPeer peer;
 
-        List<String> commands;
-        public bool Start(bool isServer)
+        bool connected;
+
+        NetConnection[] connections;
+
+        List<Command> commands;
+        List<Command> localCommands;
+        public bool Start(bool asServer)
         {
-            commands = new List<String>();
+            commands = new List<Command>();
+            localCommands = new List<Command>();
+
+            connections = new NetConnection[3];
+
+            connected = true;
+            isServer = asServer;
             if (isServer)
             {
                 Console.WriteLine("Server");
@@ -26,6 +43,7 @@ namespace TOJAM12
             }
             else
             {
+                Console.WriteLine("Client");
                 NetPeerConfiguration config = new NetPeerConfiguration("TOJAM12");
                 NetClient client = new NetClient(config);
                 client.Start();
@@ -35,23 +53,46 @@ namespace TOJAM12
             return true;
         }
 
-        public void SendCommand(String command)
+        public void SendCommand(Command command)
         {
-            foreach (NetConnection connection in peer.Connections)
+            if (!connected)
+                return;
+
+            if (isServer && (command.PlayerId == 1 || command.PlayerId == 0))
+                localCommands.Add(command);
+            
+            if (command.PlayerId != 1)
             {
-                NetOutgoingMessage sendMsg = peer.CreateMessage(command);
-                peer.SendMessage(sendMsg, connection, NetDeliveryMethod.ReliableOrdered);
+                foreach (NetConnection connection in peer.Connections)
+                {
+                    if (command.PlayerId == 0 || (command.PlayerId > 1 && connection == connections[command.PlayerId - 2]))
+                    {
+                        NetOutgoingMessage sendMsg = peer.CreateMessage();
+                        sendMsg.Write((Int32)command.Type);
+                        sendMsg.Write(command.Data);
+                        peer.SendMessage(sendMsg, connection, NetDeliveryMethod.ReliableOrdered);
+                    }
+                }
             }
         }
 
-        public List<String> GetCommands()
+        public List<Command> GetLocalCommands()
+        {
+            return localCommands;
+        }
+        public List<Command> GetCommands()
         {
             return commands;
         }
         
         public void Update()
         {
+            localCommands.Clear();
             commands.Clear();
+
+            if (!connected)
+                return;
+
             NetIncomingMessage message;
             while ((message = peer.ReadMessage()) != null)
             {
@@ -59,14 +100,44 @@ namespace TOJAM12
                 {
                     case NetIncomingMessageType.Data:
                         // handle custom messages
-                        String messageString = message.ReadString();
-                        Console.WriteLine("Message: " + messageString);
-                        commands.Add(messageString);
+                        int messageType = message.ReadInt32();
+                        String messageData = message.ReadString();
+
+                        int playerId = 0;
+                        if (isServer)
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                if (connections[i] != null && connections[i] == message.SenderConnection)
+                                {
+                                    playerId = i + 2;
+                                    break;
+                                }
+                            }
+                        }
+                        commands.Add(new Command((Command.CommandType)messageType, messageData, playerId));
+
                         break;
 
                     case NetIncomingMessageType.StatusChanged:
                         // handle connection status messages
                         Console.WriteLine(message.SenderConnection.RemoteEndPoint.Address.ToString() + ": Status Changed " + message.SenderConnection.Status.ToString());
+                        if (isServer)
+                        {
+                            if (message.SenderConnection.Status == NetConnectionStatus.Connected)
+                            {
+                                for (int i = 0; i < 3; i++)
+                                {
+                                    if (connections[i]  == null)
+                                    {
+                                        connections[i] = message.SenderConnection;
+                                        Console.WriteLine("Added connection as played id " + i);
+                                        SendCommand(new Command(Command.CommandType.Text, "Player " + message.SenderConnection.RemoteEndPoint.Address.ToString() + " joined", 0));
+                                        break;
+                                    }
+                                }
+                            }
+                        }
                         break;
 
                     case NetIncomingMessageType.DebugMessage:
